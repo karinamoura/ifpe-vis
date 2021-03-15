aae9c1baad2b0952c4d592e9fc2d0b2eef7c65c8
Karina Moura <kms2@cin.ufpe.br>
2021-03-15 00:00:47 -0300

    adicionando sankey

--- sankey.js
+++ sankey.js
@@ -1,14 +1,18 @@
-google.charts.load("current", {packages:["sankey"]});
+    google.charts.load("current", {packages:["sankey"]});
 
-$( "#palmares" ).click(function() {
-    google.charts.setOnLoadCallback(drawChartPalmares);
-  });
+    $( "#palmares" ).click(function() {
+        google.charts.setOnLoadCallback(drawChartPalmares);
+    });
 
-  $( "#recife" ).click(function() {
+    $( "#recife" ).click(function() {
+        google.charts.setOnLoadCallback(drawChart);
+    });
+
+    $( "#abreu" ).click(function() {
+        google.charts.setOnLoadCallback(drawChartAbreu);
+    });
+
     google.charts.setOnLoadCallback(drawChart);
-  });
-
-  google.charts.setOnLoadCallback(drawChart);
 
    function drawChart() {
     var data = new google.visualization.DataTable();
@@ -17,13 +21,13 @@ $( "#palmares" ).click(function() {
     data.addColumn('number', 'Weight');
     
     data.addRows([
-       [ 'CAMPUS RECIFE', 'ATÉ 1 SALÁRIO', 2434 ],
-       [ 'CAMPUS RECIFE', '1 A 2 SALÁRIOS', 33390 ],
-       [ 'CAMPUS RECIFE', '2 A 3 SALÁRIOS', 2042 ],
-       [ 'CAMPUS RECIFE', '3 A 5 SALÁRIOS', 2204 ],
-       /*[ 'CAMPUS RECIFE', '5 A 10 SALÁRIOS', 697 ],
-       [ 'CAMPUS RECIFE', '10 A 20 SALÁRIOS', 188 ],  
-       [ 'CAMPUS RECIFE', 'MAIS DE 20', 32 ],*/
+       [ 'CAMPUS RECIFE', 'ATÉ 1 SALÁRIO', 1347 ],
+       [ 'CAMPUS RECIFE', '1 A 2 SALÁRIOS', 26405 ],
+       [ 'CAMPUS RECIFE', '2 A 3 SALÁRIOS', 1498 ],
+       [ 'CAMPUS RECIFE', '3 A 5 SALÁRIOS', 1622 ],
+       [ 'CAMPUS RECIFE', '5 A 10 SALÁRIOS', 507 ],
+       [ 'CAMPUS RECIFE', '10 A 20 SALÁRIOS', 129 ],  
+       [ 'CAMPUS RECIFE', 'MAIS DE 20', 24],
        
        [ 'ATÉ 1 SALÁRIO', 'PRIVADA', 382],
        [ 'ATÉ 1 SALÁRIO', 'PÚBLICA ESTADUAL', 1240],
@@ -45,139 +49,166 @@ $( "#palmares" ).click(function() {
        [ '3 A 5 SALÁRIOS', 'PÚBLICA FEDERAL', 168],
        [ '3 A 5 SALÁRIOS', 'PÚBLICA MUNICIPAL', 66],
 
-       [ 'PRIVADA', 'ABANDONO/EVASÃO', 2203],
-       [ 'PÚBLICA ESTADUAL', 'ABANDONO/EVASÃO', 15246],
-       [ 'PÚBLICA FEDERAL', 'ABANDONO/EVASÃO', 354],
-       [ 'PÚBLICA MUNICIPAL', 'ABANDONO/EVASÃO', 232],
+       [ '5 A 10 SALÁRIOS', 'PRIVADA', 531],
+       [ '5 A 10 SALÁRIOS', 'PÚBLICA ESTADUAL', 118],
+       [ '5 A 10 SALÁRIOS', 'PÚBLICA FEDERAL', 64],
+       [ '5 A 10 SALÁRIOS', 'PÚBLICA MUNICIPAL', 8],
 
-       [ 'PRIVADA', 'CONCLUIDO', 1495],
-       [ 'PÚBLICA ESTADUAL', 'CONCLUIDO', 10870],
-       [ 'PÚBLICA FEDERAL', 'CONCLUIDO', 324],
-       [ 'PÚBLICA MUNICIPAL', 'CONCLUIDO', 131],
+       [ '10 A 20 SALÁRIOS', 'PRIVADA', 155],
+       [ '10 A 20 SALÁRIOS', 'PÚBLICA ESTADUAL', 26],
+       [ '10 A 20 SALÁRIOS', 'PÚBLICA FEDERAL', 9],
+       [ '10 A 20 SALÁRIOS', 'PÚBLICA MUNICIPAL', 5],
+
+       [ 'MAIS DE 20', 'PRIVADA', 29],
+       [ 'MAIS DE 20', 'PÚBLICA ESTADUAL', 4],
+       [ 'MAIS DE 20', 'PÚBLICA FEDERAL', 3],
+       [ 'MAIS DE 20', 'PÚBLICA MUNICIPAL', 0],
+
+       [ 'PRIVADA', 'ABANDONO/EVASÃO', 3897],
+       [ 'PÚBLICA ESTADUAL', 'ABANDONO/EVASÃO', 16905],
+       [ 'PÚBLICA FEDERAL', 'ABANDONO/EVASÃO', 534],
+       [ 'PÚBLICA MUNICIPAL', 'ABANDONO/EVASÃO', 699],
+
+       [ 'PRIVADA', 'CONCLUIDO', 1996],
+       [ 'PÚBLICA ESTADUAL', 'CONCLUIDO', 11183],
+       [ 'PÚBLICA FEDERAL', 'CONCLUIDO', 410],
+       [ 'PÚBLICA MUNICIPAL', 'CONCLUIDO', 182],
+
+       [ 'PRIVADA', 'MATRICULADO', 1241],
+       [ 'PÚBLICA ESTADUAL', 'MATRICULADO', 7538],
+       [ 'PÚBLICA FEDERAL', 'MATRICULADO', 186],
+       [ 'PÚBLICA MUNICIPAL', 'MATRICULADO', 359],
 
 
     ]);
 
+    // Set chart options
+    var options = {
+      width: 1400,
+
+      sankey: { node: { nodePadding: 25, width: 20 } },
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
+    chart.draw(data, options);
+   }
+
+    function drawChartPalmares() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'From');
+        data.addColumn('string', 'To');
+        data.addColumn('number', 'Weight');   
+        data.addRows([
+            [ 'CAMPUS PALMARES', 'ATÉ 1 SALÁRIO', 175 ],
+            [ 'CAMPUS PALMARES', '1 A 2 SALÁRIOS', 91 ],
+            [ 'CAMPUS PALMARES', '2 A 3 SALÁRIOS', 9 ],
+            [ 'CAMPUS PALMARES', '3 A 5 SALÁRIOS', 3 ],
+            [ 'CAMPUS PALMARES', '5 A 10 SALÁRIOS', 0 ],
+            [ 'CAMPUS PALMARES', '10 A 20 SALÁRIOS', 0 ],  
+            [ 'CAMPUS PALMARES', 'MAIS DE 20', 0 ],
+            
+            [ 'ATÉ 1 SALÁRIO', 'PRIVADA', 3],
+            [ 'ATÉ 1 SALÁRIO', 'PÚBLICA ESTADUAL', 62],
+            [ 'ATÉ 1 SALÁRIO', 'PÚBLICA MUNICIPAL', 7],
     
-    /*data.addRows([
-       [ 'CAMPUS RECIFE', 'PRIVADA', 4602 ],
-       [ 'CAMPUS RECIFE', 'PÚBLICA ESTADUAL', 5404 ],
-       [ 'CAMPUS RECIFE', 'PÚBLICA FEDERAL', 748 ],
-       [ 'CAMPUS RECIFE', 'PÚBLICA MUNICIPAL', 1104 ],
-       
-       [ 'PRIVADA', 'CONCLUÍDO', 869],
-       [ 'PÚBLICA ESTADUAL', 'CONCLUÍDO', 803],
-       [ 'PÚBLICA FEDERAL', 'CONCLUÍDO', 208],
-       [ 'PÚBLICA MUNICIPAL', 'CONCLUÍDO', 101],
+            [ '1 A 2 SALÁRIOS', 'PRIVADA', 2],
+            [ '1 A 2 SALÁRIOS', 'PÚBLICA ESTADUAL', 52],
+        
+            [ '1 A 2 SALÁRIOS', 'PÚBLICA MUNICIPAL', 8],
+    
+            [ '2 A 3 SALÁRIOS', 'PRIVADA', 0],
+            [ '2 A 3 SALÁRIOS', 'PÚBLICA ESTADUAL', 2],
+            [ '2 A 3 SALÁRIOS', 'PÚBLICA MUNICIPAL', 0],
+    
+            [ '3 A 5 SALÁRIOS', 'PRIVADA', 2],
+            [ '3 A 5 SALÁRIOS', 'PÚBLICA ESTADUAL', 1],
+            [ '3 A 5 SALÁRIOS', 'PÚBLICA MUNICIPAL', 0],
+    
+            [ 'PRIVADA', 'ABANDONO/EVASÃO', 23],
+            [ 'PÚBLICA ESTADUAL', 'ABANDONO/EVASÃO', 323],
+            [ 'PÚBLICA FEDERAL', 'ABANDONO/EVASÃO', 15],
+            [ 'PÚBLICA MUNICIPAL', 'ABANDONO/EVASÃO', 16],
+    
+            [ 'PRIVADA', 'CONCLUIDO', 15],
+            [ 'PÚBLICA ESTADUAL', 'CONCLUIDO', 154],
+            [ 'PÚBLICA FEDERAL', 'CONCLUIDO', 4],
+            [ 'PÚBLICA MUNICIPAL', 'CONCLUIDO', 9],
 
-       [ 'PRIVADA', 'ABANDONO/EVASÃO', 2383],
-       [ 'PÚBLICA ESTADUAL', 'ABANDONO/EVASÃO', 2982],
-       [ 'PÚBLICA FEDERAL', 'ABANDONO/EVASÃO', 358],
-       [ 'PÚBLICA MUNICIPAL', 'ABANDONO/EVASÃO', 605],
+            [ 'PRIVADA', 'MATRICULADO', 5],
+            [ 'PÚBLICA ESTADUAL', 'MATRICULADO', 49],
+            [ 'PÚBLICA FEDERAL', 'MATRICULADO', 1],
+            [ 'PÚBLICA MUNICIPAL', 'MATRICULADO', 5],
 
-       [ 'PRIVADA', 'FORMADO', 358],
-       [ 'PÚBLICA ESTADUAL', 'FORMADO', 333],
-       [ 'PÚBLICA FEDERAL', 'FORMADO', 61],
-       [ 'PÚBLICA MUNICIPAL', 'FORMADO', 112],
+        ]);
 
-       [ 'ABANDONO/EVASÃO', '1 A 2 SALÁRIOS', 758],
-       [ 'CONCLUÍDO', '1 A 2 SALÁRIOS', 278],
-       [ 'FORMADO', '1 A 2 SALÁRIOS', 67],
+        // Set chart options
+        var options = {
+    
+        sankey: { node: { nodePadding: 30 } },
+        };
 
-       [ 'ABANDONO/EVASÃO', '2 A 3 SALÁRIOS', 227],
-       [ 'CONCLUÍDO', '2 A 3 SALÁRIOS', 131],
-       [ 'FORMADO', '2 A 3 SALÁRIOS', 67],
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
+        chart.draw(data, options);
 
-    ]);*/
+    }
 
+    function drawChartAbreu() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'From');
+        data.addColumn('string', 'To');
+        data.addColumn('number', 'Weight');   
+        data.addRows([
+            [ 'CAMPUS ABREU E LIMA', 'ATÉ 1 SALÁRIO', 295 ],
+            [ 'CAMPUS ABREU E LIMA', '1 A 2 SALÁRIOS', 466 ],
+            [ 'CAMPUS ABREU E LIMA', '2 A 3 SALÁRIOS', 59 ],
+            [ 'CAMPUS ABREU E LIMA', '3 A 5 SALÁRIOS', 40 ],
+            [ 'CAMPUS ABREU E LIMA', '5 A 10 SALÁRIOS', 10 ], 
+            
+            [ 'ATÉ 1 SALÁRIO', 'PRIVADA', 2],
+            [ 'ATÉ 1 SALÁRIO', 'PÚBLICA ESTADUAL', 145],
+            [ 'ATÉ 1 SALÁRIO', 'PÚBLICA MUNICIPAL', 13],
+     
+            [ '1 A 2 SALÁRIOS', 'PRIVADA', 16],
+            [ '1 A 2 SALÁRIOS', 'PÚBLICA ESTADUAL', 137],
+            [ '1 A 2 SALÁRIOS', 'PÚBLICA FEDERAL', 1],
+            [ '1 A 2 SALÁRIOS', 'PÚBLICA MUNICIPAL', 11],
+     
+            [ '2 A 3 SALÁRIOS', 'PRIVADA', 11],
+            [ '2 A 3 SALÁRIOS', 'PÚBLICA ESTADUAL', 11],
+            [ '2 A 3 SALÁRIOS', 'PÚBLICA MUNICIPAL', 3],
+     
+            [ '3 A 5 SALÁRIOS', 'PRIVADA', 11],
+            [ '3 A 5 SALÁRIOS', 'PÚBLICA ESTADUAL', 15],
+            [ '3 A 5 SALÁRIOS', 'PÚBLICA FEDERAL', 1],
 
-    /*data.addRows([
-       [ 'ABREU E LIMA', 'PRIVADA', 559 ],
-       [ 'ABREU E LIMA', 'PÚBLICA ESTADUAL', 3689 ],
-       [ 'ABREU E LIMA', 'PÚBLICA FEDERAL', 102 ],
-       [ 'ABREU E LIMA', 'PÚBLICA MUNICIPAL', 320 ],
-       
-       [ 'PRIVADA', 'APROVADO', 462],
-       [ 'PÚBLICA ESTADUAL', 'APROVADO', 2214],
-       [ 'PÚBLICA FEDERAL', 'APROVADO', 87],
-       [ 'PÚBLICA MUNICIPAL', 'APROVADO', 223],
-       [ 'PRIVADA', 'REPROVADO', 16],
-       [ 'PÚBLICA ESTADUAL', 'REPROVADO', 222],
-       [ 'PÚBLICA FEDERAL', 'REPROVADO', 14],
-       [ 'PÚBLICA MUNICIPAL', 'REPROVADO', 23]
-    ]); */
+            [ '5 A 10 SALÁRIOS', 'PÚBLICA ESTADUAL', 1],
+     
+            [ 'PRIVADA', 'ABANDONO/EVASÃO', 5],
+            [ 'PÚBLICA ESTADUAL', 'ABANDONO/EVASÃO', 27],
+            [ 'PÚBLICA FEDERAL', 'ABANDONO/EVASÃO', 0],
+            [ 'PÚBLICA MUNICIPAL', 'ABANDONO/EVASÃO', 3],
+     
+            [ 'PRIVADA', 'CONCLUIDO', 13],
+            [ 'PÚBLICA ESTADUAL', 'CONCLUIDO', 69],
+            [ 'PÚBLICA FEDERAL', 'CONCLUIDO', 2],
+            [ 'PÚBLICA MUNICIPAL', 'CONCLUIDO', 7],
 
-    // Set chart options
-    var options = {
-      width: 900,
-      sankey: { node: { nodePadding: 50 } },
-    };
+            [ 'PRIVADA', 'MATRICULADO', 21],
+            [ 'PÚBLICA ESTADUAL', 'MATRICULADO', 215],
+            [ 'PÚBLICA FEDERAL', 'MATRICULADO', 3],
+            [ 'PÚBLICA MUNICIPAL', 'MATRICULADO', 16],
+    
+        ]);
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
-    chart.draw(data, options);
-   }
+        // Set chart options
+        var options = {
+    
+        sankey: { node: { nodePadding: 30 } },
+        };
 
-   function drawChartPalmares() {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'From');
-    data.addColumn('string', 'To');
-    data.addColumn('number', 'Weight');   
-    data.addRows([
-       [ 'CAMPUS RECIFE', 'PRIVADA', 4602 ],
-       [ 'CAMPUS RECIFE', 'PÚBLICA ESTADUAL', 5404 ],
-       [ 'CAMPUS RECIFE', 'PÚBLICA FEDERAL', 748 ],
-       [ 'CAMPUS RECIFE', 'PÚBLICA MUNICIPAL', 1104 ],
-       
-       [ 'PRIVADA', 'CONCLUÍDO', 869],
-       [ 'PÚBLICA ESTADUAL', 'CONCLUÍDO', 803],
-       [ 'PÚBLICA FEDERAL', 'CONCLUÍDO', 208],
-       [ 'PÚBLICA MUNICIPAL', 'CONCLUÍDO', 101],
-
-       [ 'PRIVADA', 'ABANDONO/EVASÃO', 2383],
-       [ 'PÚBLICA ESTADUAL', 'ABANDONO/EVASÃO', 2982],
-       [ 'PÚBLICA FEDERAL', 'ABANDONO/EVASÃO', 358],
-       [ 'PÚBLICA MUNICIPAL', 'ABANDONO/EVASÃO', 605],
-
-       [ 'PRIVADA', 'FORMADO', 358],
-       [ 'PÚBLICA ESTADUAL', 'FORMADO', 333],
-       [ 'PÚBLICA FEDERAL', 'FORMADO', 61],
-       [ 'PÚBLICA MUNICIPAL', 'FORMADO', 112],
-
-       [ 'ABANDONO/EVASÃO', '1 A 2 SALÁRIOS', 758],
-       [ 'CONCLUÍDO', '1 A 2 SALÁRIOS', 278],
-       [ 'FORMADO', '1 A 2 SALÁRIOS', 67],
-
-       [ 'ABANDONO/EVASÃO', '2 A 3 SALÁRIOS', 227],
-       [ 'CONCLUÍDO', '2 A 3 SALÁRIOS', 131],
-       [ 'FORMADO', '2 A 3 SALÁRIOS', 67],
-
-    ]);
-
-
-    /*data.addRows([
-       [ 'ABREU E LIMA', 'PRIVADA', 559 ],
-       [ 'ABREU E LIMA', 'PÚBLICA ESTADUAL', 3689 ],
-       [ 'ABREU E LIMA', 'PÚBLICA FEDERAL', 102 ],
-       [ 'ABREU E LIMA', 'PÚBLICA MUNICIPAL', 320 ],
-       
-       [ 'PRIVADA', 'APROVADO', 462],
-       [ 'PÚBLICA ESTADUAL', 'APROVADO', 2214],
-       [ 'PÚBLICA FEDERAL', 'APROVADO', 87],
-       [ 'PÚBLICA MUNICIPAL', 'APROVADO', 223],
-       [ 'PRIVADA', 'REPROVADO', 16],
-       [ 'PÚBLICA ESTADUAL', 'REPROVADO', 222],
-       [ 'PÚBLICA FEDERAL', 'REPROVADO', 14],
-       [ 'PÚBLICA MUNICIPAL', 'REPROVADO', 23]
-    ]); */
-
-    // Set chart options
-    var options = {
-      width: 900,
-      sankey: { node: { nodePadding: 50 } },
-    };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
-    chart.draw(data, options);
-   }
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
+        chart.draw(data, options);
+    }
